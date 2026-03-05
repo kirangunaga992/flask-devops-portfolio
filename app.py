07cfaedaed3b99494fac96671ca5a40cc970c356
Rohan Gunaga <kirangunaga992@gmail.com>
2026-03-05 10:47:24 +0000

    feat: added secure dashboard with interactive pie chart

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = 'devops_is_awesome' # Required for sessions
 
 @app.route('/')
 def home():
-    # Points to the new HTML file in the /templates folder
     return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # This is a simple check to practice handling 'POST' requests
-        username = request.form.get('username')
-        return f"<h1>Security Alert:</h1><p>Attempted login for user: {username}</p>"
+        # For now, any login works! In the future, you can add a real password.
+        session['user'] = request.form.get('username')
+        return redirect(url_for('dashboard'))
     return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
